66dfc14feb7fbf278ae1830bc9071d8337297624
Marcelo Mansilla <marcmansilla@uade.edu.ar>
2025-03-19 17:02:46 -0300

    agrego validaciones

--- src/tp1/MatrizIngresoDatos.java
+++ src/tp1/MatrizIngresoDatos.java
@@ -6,18 +6,35 @@ public class MatrizIngresoDatos {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Ingrese el número de filas:");
-        int filas = scanner.nextInt();
-        System.out.println("Ingrese el número de columnas:");
-        int columnas = scanner.nextInt();
+        int filas, columnas;
+        do {
+            System.out.println("Ingrese el número de filas (debe ser positivo):");
+            filas = scanner.nextInt();
+            if (filas <= 0) {
+                System.out.println("Error: El número debe ser positivo");
+            }
+        } while (filas <= 0);
+        
+        do {
+            System.out.println("Ingrese el número de columnas (debe ser positivo):");
+            columnas = scanner.nextInt();
+            if (columnas <= 0) {
+                System.out.println("Error: El número debe ser positivo");
+            }
+        } while (columnas <= 0);
         
         int[][] matriz = new int[filas][columnas];
         
-        System.out.println("Ingrese los elementos de la matriz:");
+        System.out.println("Ingrese los elementos de la matriz (solo positivos):");
         for (int i = 0; i < filas; i++) {
             for (int j = 0; j < columnas; j++) {
-                System.out.printf("Ingrese el elemento [%d][%d]: ", i, j);
-                matriz[i][j] = scanner.nextInt();
+                do {
+                    System.out.printf("Ingrese el elemento [%d][%d]: ", i, j);
+                    matriz[i][j] = scanner.nextInt();
+                    if (matriz[i][j] <= 0) {
+                        System.out.println("Error: El número debe ser positivo");
+                    }
+                } while (matriz[i][j] <= 0);
             }
         }
 
